cee65d4b8203a355df0f39dbaf777734032855c6
one_sujeet <sujeet@apoxeo.com>
2019-01-23 12:57:54 +0530

    Initial Commit

---
+++ app/src/main/java/me/pickers/DatePicker.java
@@ -0,0 +1,122 @@
+package me.pickers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.shawnlin.numberpicker.NumberPicker;
+
+import java.util.Calendar;
+
+import static me.pickers.ExpiryPicker.getCurrentMonth;
+
+public class DatePicker extends AppCompatActivity {
+
+    View picker;
+    Button pickdate;
+    NumberPicker day, month, year;
+    Calendar calendar;
+    public static String[] MONTHS = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_picker);
+
+        picker = findViewById(R.id.dp_picker);
+        pickdate = findViewById(R.id.dp_pick);
+
+        day = picker.findViewById(R.id.day);
+        month = picker.findViewById(R.id.month);
+        year = picker.findViewById(R.id.year);
+
+        calendar = Calendar.getInstance();
+
+        day.setMinValue(1);
+        year.setMinValue(1900);
+
+        year.setMaxValue(calendar.get(Calendar.YEAR));
+        month.setMaxValue(selectMonthMaxValue(calendar, year.getValue()));
+        day.setMaxValue(selectDayMaxValue(calendar, year.getValue(), month.getValue()));
+
+        year.setValue(1991);
+        month.setValue(1);
+        day.setValue(1);
+
+        month.setDisplayedValues(MONTHS);
+
+        year.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                day.setMaxValue(selectDayMaxValue(calendar, newVal, month.getValue()));
+                month.setMaxValue(selectMonthMaxValue(calendar, newVal));
+            }
+        });
+
+        month.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                day.setMaxValue(selectDayMaxValue(calendar, year.getValue(), newVal));
+            }
+        });
+
+        pickdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Selected Date is: " +
+                        day.getValue() + "-" + month.getValue() + "-" + year.getValue(), Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+
+    public static int selectMonthMaxValue(Calendar calendar, int yearValue) {
+        if (yearValue == calendar.get(Calendar.YEAR)) {
+            return getCurrentMonth(calendar.get(Calendar.MONTH));
+        }
+        return 12;
+    }
+
+    public static int selectDayMaxValue(Calendar calendar, int yearValue, int monthValue) {
+        if (yearValue == calendar.get(Calendar.YEAR) && monthValue == getCurrentMonth(calendar.get(Calendar.MONTH))) {
+            return calendar.get(Calendar.DAY_OF_MONTH);
+        }
+        return pickMonthMaxDays(yearValue, monthValue);
+    }
+
+    public static int pickMonthMaxDays(int year, int month) {
+        switch (month) {
+            case 1:
+                return 31;
+            case 2:
+                if (year % 4 == 0) {
+                    return 29;
+                } else {
+                    return 28;
+                }
+            case 3:
+                return 31;
+            case 4:
+                return 30;
+            case 5:
+                return 31;
+            case 6:
+                return 30;
+            case 7:
+                return 31;
+            case 8:
+                return 31;
+            case 9:
+                return 30;
+            case 10:
+                return 31;
+            case 11:
+                return 30;
+            case 12:
+                return 31;
+        }
+        return 30;
+    }
+}
---
+++ app/src/main/java/me/pickers/ExpiryPicker.java
@@ -0,0 +1,100 @@
+package me.pickers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.shawnlin.numberpicker.NumberPicker;
+
+import java.util.Calendar;
+
+import static me.pickers.DatePicker.MONTHS;
+import static me.pickers.DatePicker.pickMonthMaxDays;
+
+public class ExpiryPicker extends AppCompatActivity {
+
+    View picker;
+    Button pickdate;
+    NumberPicker day, month, year;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_picker);
+
+        picker = findViewById(R.id.dp_picker);
+        pickdate = findViewById(R.id.dp_pick);
+
+        day = picker.findViewById(R.id.day);
+        month = picker.findViewById(R.id.month);
+        year = picker.findViewById(R.id.year);
+
+        calendar = Calendar.getInstance();
+
+        day.setMinValue(selectDayMinValue(calendar, calendar.get(Calendar.YEAR), getCurrentMonth(calendar.get(Calendar.MONTH))));
+        year.setMinValue(calendar.get(Calendar.YEAR));
+
+        day.setMaxValue(pickMonthMaxDays(calendar.get(Calendar.YEAR), getCurrentMonth(calendar.get(Calendar.MONTH))));
+        year.setMaxValue(2099);
+        month.setMaxValue(12);
+
+        year.setValue(calendar.get(Calendar.YEAR));
+        month.setValue(getCurrentMonth(calendar.get(Calendar.MONTH)));
+        day.setValue(calendar.get(Calendar.DAY_OF_MONTH));
+
+        month.setDisplayedValues(MONTHS);
+
+        year.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                month.setMinValue(selectMonthMinValue(calendar, newVal));
+                day.setMinValue(selectDayMinValue(calendar, newVal, month.getValue()));
+                day.setMaxValue(pickMonthMaxDays(calendar.get(Calendar.YEAR), getCurrentMonth(calendar.get(Calendar.MONTH))));
+            }
+        });
+
+        month.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                day.setMinValue(selectDayMinValue(calendar, year.getValue(), newVal));
+                day.setMaxValue(pickMonthMaxDays(calendar.get(Calendar.YEAR), getCurrentMonth(calendar.get(Calendar.MONTH))));
+            }
+        });
+
+        pickdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Selected Date is: " +
+                        day.getValue() + "-" + month.getValue() + "-" + year.getValue(), Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+
+    //for some reason calendar is returning 0 for JAN, 1 for FEB and so on...
+    public static int getCurrentMonth(int month) {
+        if (month != 12) {
+            month++;
+            return month;
+        }
+        return month;
+    }
+
+    int selectDayMinValue(Calendar calendar, int year, int month) {
+        if (year == calendar.get(Calendar.YEAR) && month == getCurrentMonth(calendar.get(Calendar.MONTH))) {
+            return calendar.get(Calendar.DAY_OF_MONTH);
+        }
+        return 1;
+    }
+
+    int selectMonthMinValue(Calendar calendar, int year) {
+        if (year == calendar.get(Calendar.YEAR)) {
+            return getCurrentMonth(calendar.get(Calendar.MONTH));
+        }
+        return 1;
+    }
+
+}
