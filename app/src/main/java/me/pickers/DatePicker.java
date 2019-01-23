335d3d0b00b5e54abafffc797f4a6093d0dc2d14
one_sujeet <sujeet@apoxeo.com>
2019-01-23 16:31:06 +0530

    Committing Again

--- app/src/main/java/me/pickers/DatePicker.java
+++ app/src/main/java/me/pickers/DatePicker.java
@@ -50,8 +50,8 @@ public class DatePicker extends AppCompatActivity {
         year.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                day.setMaxValue(selectDayMaxValue(calendar, newVal, month.getValue()));
                 month.setMaxValue(selectMonthMaxValue(calendar, newVal));
+                day.setMaxValue(selectDayMaxValue(calendar, newVal, month.getValue()));
             }
         });
 
@@ -88,35 +88,22 @@ public class DatePicker extends AppCompatActivity {
 
     public static int pickMonthMaxDays(int year, int month) {
         switch (month) {
-            case 1:
-                return 31;
             case 2:
                 if (year % 4 == 0) {
                     return 29;
                 } else {
                     return 28;
                 }
-            case 3:
-                return 31;
             case 4:
                 return 30;
-            case 5:
-                return 31;
             case 6:
                 return 30;
-            case 7:
-                return 31;
-            case 8:
-                return 31;
             case 9:
                 return 30;
-            case 10:
-                return 31;
             case 11:
                 return 30;
-            case 12:
+            default:
                 return 31;
         }
-        return 30;
     }
 }
